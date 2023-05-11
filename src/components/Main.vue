0fe83fd9e6d464eedba80d4b73da4bbeedb9e586
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-11 03:34:49 +0200

    left-container Styling

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="jumbotron">
   <div class="left-section">
-    <h1>Titolo</h1>
-    <p>Primo paragrafo</p>
-    <button type="button">Bottone</button>
+    <h1>Start Investing in Yourself</h1>
+    <p>
+      With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you.
+    </p>
+    <button type="button">Join for Free</button>
   </div>
   <div class="right-section">
     <img class="base-image" src="src/assets/img/base.png" alt="Immagine base">
